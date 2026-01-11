747ea70a40a4c51f39a2cdff87b514d104341b7a
Eduard Bryan P. Rivera <edubprivera@gmail.com>
2026-01-11 16:33:23 +0800

    Add auth guard to comments routes

--- src/posts/comments/comments.controller.ts
+++ src/posts/comments/comments.controller.ts
@@ -1,15 +1,32 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+  Request
+} from '@nestjs/common';
 import { CommentsService } from './comments.service';
-import { UpdateCommentDto } from './dto/update-comment.dto';
 import { Prisma } from 'generated/prisma/client';
+import { JwtAuthGuard } from 'src/auth/jwt.guard';
+
+import type { IUserRequest } from 'src/auth/jwt.guard';
 
 @Controller()
 export class CommentsController {
   constructor(private readonly commentsService: CommentsService) { }
 
+  @UseGuards(JwtAuthGuard)
   @Post()
-  createPostComment(@Param('post_id') postId: string, @Body() commentInput: Prisma.CommentCreateInput) {
-    return this.commentsService.createPostComment(+postId, commentInput);
+  createPostComment(
+    @Param('post_id') postId: string,
+    @Body() commentInput: Prisma.CommentCreateInput,
+    @Request() request: IUserRequest
+  ) {
+    return this.commentsService.createPostComment(+postId, request.user.id, commentInput);
   }
 
   @Get()
@@ -22,13 +39,19 @@ export class CommentsController {
     return this.commentsService.findOne(+commentId);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Patch(':comment_id')
-  update(@Param('comment_id') id: string, @Body() updateInput: Prisma.CommentUpdateInput) {
-    return this.commentsService.update(+id, updateInput);
+  update(
+    @Param('comment_id') id: string,
+    @Body() updateInput: Prisma.CommentUpdateInput,
+    @Request() request: IUserRequest
+  ) {
+    return this.commentsService.update(+id, request.user.id, updateInput);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':comment_id')
-  remove(@Param('comment_id') id: string) {
-    return this.commentsService.remove(+id);
+  remove(@Param('comment_id') id: string, @Request() request: IUserRequest) {
+    return this.commentsService.remove(+id, request.user.id);
   }
 }
